eb338b73a079641cc93e79678e4f46fc988ed58a
omar oulhaj <omaroulhaj19@gmail.com>
2024-12-13 12:35:05 +0100

    Income Data update and delete

--- app/src/main/java/com/example/flousino/AjouterFragment.java
+++ app/src/main/java/com/example/flousino/AjouterFragment.java
@@ -1,5 +1,6 @@
 package com.example.flousino;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,15 +11,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.flousino.Model.Data;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -72,7 +82,19 @@ public class AjouterFragment extends Fragment {
     private DatabaseReference mIncomeDatabase;
     private FirebaseRecyclerAdapter adapter;
 
-
+    private TextView incomeTotalSum;
+    // Update edit text
+    private EditText ammountEditText;
+    private EditText typeEditText;
+    private EditText noteEditText;
+    // button for update and delete
+    private Button btnUpdate;
+    private Button btnDelete;
+    // Data item value to update
+    private int amount;
+    private String type;
+    private String note;
+    private String post_key;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -82,7 +104,7 @@ public class AjouterFragment extends Fragment {
         mAuth=FirebaseAuth.getInstance();
         FirebaseUser mUser=mAuth.getCurrentUser();
         String uid=mUser.getUid();
-
+        incomeTotalSum=myview.findViewById(R.id.income_txt_result);
         mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
         recyclerView=myview.findViewById(R.id.recycler_id_income);
         LinearLayoutManager layoutManager=new LinearLayoutManager(getActivity());
@@ -91,6 +113,26 @@ public class AjouterFragment extends Fragment {
         layoutManager.setStackFromEnd(true);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
+        mIncomeDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int totalValue = 0;
+                for(DataSnapshot mysnapshot:dataSnapshot.getChildren())
+                {
+                    Data data=mysnapshot.getValue(Data.class);
+                    totalValue+=data.getAmount();
+                    String stTotalValue=String.valueOf(totalValue);
+                    incomeTotalSum.setText(stTotalValue+" DH");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
 
         return myview;
@@ -122,6 +164,17 @@ public class AjouterFragment extends Fragment {
                 holder.setType(model.getType());
                 holder.setNote(model.getNote());
                 holder.setDate(model.getDate());
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        post_key=getRef(position).getKey();
+                        amount=model.getAmount();
+                        type=model.getType();
+                        note=model.getNote();
+                        updateDataItem();
+                    }
+                });
+
             }
         };
 
@@ -139,7 +192,63 @@ public class AjouterFragment extends Fragment {
             adapter.stopListening();
         }
     }
+    private void updateDataItem()
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=getLayoutInflater();
+        View myview=inflater.inflate(R.layout.update_data_item,null);
+        builder.setView(myview);
+        ammountEditText=myview.findViewById(R.id.amount_edt);
+        typeEditText=myview.findViewById(R.id.type_edt);
+        noteEditText=myview.findViewById(R.id.note_edt);
 
+        btnUpdate=myview.findViewById(R.id.btnUpdate);
+        btnDelete=myview.findViewById(R.id.btnDelete);
+        AlertDialog dialog=builder.create();
+        ammountEditText.setText(String.valueOf(amount));
+        typeEditText.setText(type);
+        typeEditText.setSelection(type.length());
+        noteEditText.setText(note);
+        noteEditText.setSelection(note.length());
+        ammountEditText.setSelection(String.valueOf(amount).length());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                type = typeEditText.getText().toString().trim();
+                note = noteEditText.getText().toString().trim();
+                String mdammount =String.valueOf(amount);
+                mdammount=ammountEditText.getText().toString().trim();
+                int amount=Integer.parseInt(mdammount);
+                String mDate = DateFormat.getDateInstance().format(new Date());
+                Data data=new Data(amount,type,note,mDate,post_key);
+                mIncomeDatabase.child(post_key).setValue(data, (error, ref) -> {
+                    if (error == null) {
+                        Toast.makeText(getActivity(), "Data Updated Successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Failed to update income data: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.dismiss();
+
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIncomeDatabase.child(post_key).removeValue((error, ref) -> {
+                    if (error == null) {
+                        Toast.makeText(getActivity(), "data Deleted successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Failed to delete data: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
 }
 
 
--- app/src/main/java/com/example/flousino/ExpenseFragment.java
+++ app/src/main/java/com/example/flousino/ExpenseFragment.java
@@ -2,11 +2,26 @@ package com.example.flousino;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.flousino.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +70,132 @@ public class ExpenseFragment extends Fragment {
         }
     }
 
+
+    // Firebase database
+    private FirebaseAuth mAuth;
+    private RecyclerView recyclerView;
+    private DatabaseReference mExpenseDatabase;
+    private FirebaseRecyclerAdapter adapter;
+
+    private TextView expenseTotalSum;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expense, container, false);
+        View myview = inflater.inflate(R.layout.fragment_expense, container, false);
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser mUser=mAuth.getCurrentUser();
+        String uid=mUser.getUid();
+        expenseTotalSum=myview.findViewById(R.id.expense_txt_result);
+        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+        recyclerView=myview.findViewById(R.id.recycler_id_expense);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getActivity());
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        mExpenseDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int totalValue = 0;
+                for(DataSnapshot mysnapshot:dataSnapshot.getChildren())
+                {
+                    Data data=mysnapshot.getValue(Data.class);
+                    totalValue+=data.getAmount();
+                    String stTotalValue=String.valueOf(totalValue);
+                    expenseTotalSum.setText(stTotalValue+" DH");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+        return myview;
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Data> options = new FirebaseRecyclerOptions.Builder<Data>()
+                .setQuery(mExpenseDatabase, Data.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<Data, MyViewHoldere>(options) {
+
+            @NonNull
+            @Override
+            public MyViewHoldere onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                // Inflate the layout for each item in the RecyclerView
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.expense_recycler_data, parent, false);
+                return new MyViewHoldere(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHoldere holder, int position, @NonNull Data model) {
+                // Bind the data from the model to the view holder
+                holder.setAmount(model.getAmount());
+                holder.setType(model.getType());
+                holder.setNote(model.getNote());
+                holder.setDate(model.getDate());
+            }
+        };
+
+        // Set the adapter to the RecyclerView
+        recyclerView.setAdapter(adapter);
+
+        // Start listening for changes in Firebase
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (adapter != null) {
+            adapter.stopListening();
+        }
+    }
+
+}
+
+
+class MyViewHoldere extends RecyclerView.ViewHolder
+{
+    View mView;
+    public MyViewHoldere(@NonNull View itemView) {
+        super(itemView);
+        mView = itemView;
+    }
+    void setDate(String date)
+    {
+        TextView mDate=mView.findViewById(R.id.date_txt_expense);
+        mDate.setText(date);
+    }
+    void setType(String type)
+    {
+        TextView mType=mView.findViewById(R.id.type_txt_expense);
+        mType.setText(type);
+    }
+    void setAmount(int amount)
+    {
+        TextView mAmount=mView.findViewById(R.id.ammount_txt_expense);
+        String stammount=String.valueOf(amount);
+        mAmount.setText(stammount);
+    }
+    void setNote(String note)
+    {
+        TextView mNote=mView.findViewById(R.id.note_txt_expense);
+        mNote.setText(note);
+    }
+
+
 }
